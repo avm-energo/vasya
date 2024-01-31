6fd6e05b2101b21b814730ae446b7b2078aad9a8
dragovx <dragovx@mail.ru>
2024-01-31 17:01:11 +0300

    Исправление ошибки, которая после закрытия проекта не останавливала работу проекта на хостинге v2

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,35 @@
 module.exports = {
   devServer: {
     allowedHosts: 'all',
-    headers: { "Cache-Control": "no-store" }
+    headers: { 'Cache-Control': 'no-store' }
   },
-  // chainWebpack: config => {			
-  //   if (process.env.NODE_ENV === 'production') {			
-  //     config.module.rule('vue').uses.delete('cache-loader');			
-  //     config.module.rule('js').uses.delete('cache-loader');			
-  //     config.module.rule('ts').uses.delete('cache-loader');			
-  //     config.module.rule('tsx').uses.delete('cache-loader');			
-  //   }			
-  // }
+  chainWebpack: config => {			
+    if (process.env.NODE_ENV === 'production') {			
+      config.module.rule('vue').uses.delete('cache-loader');			
+      config.module.rule('js').uses.delete('cache-loader');			
+      config.module.rule('ts').uses.delete('cache-loader');			
+      config.module.rule('tsx').uses.delete('cache-loader');			
+    }			
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.splitChunks.cacheGroups = {
+        default: false,
+        vendors: false,
+        // Создаем новый кэш для статических файлов
+        assets: {
+          test: /[\\/](js|css|png|jpg)$/,
+          name: 'static',
+          chunks: 'all',
+          priority: 20,
+          enforce: true,
+        },
+      };
+      
+      config.output.filename = '[name].[contenthash].js'; // Добавляем contenthash к именам файлов
+      config.output.chunkFilename = '[name].[contenthash].js'; // Обрабатываем chunk файлы аналогично
+    }
+  },
   // configureWebpack: {
   //       devServer: {
   //           host: 'mycustomdomain',
