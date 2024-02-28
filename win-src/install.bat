a691c12266d02796b869c582ed8b3c59c4d7fd5d
Evel <vao@asu-vei.ru>
2024-02-28 13:20:16 +0300

    [*] install.bat changed to unzip nginx in the systems that doesn't have tar archiver|

--- win-src/install.bat
+++ win-src/install.bat
@@ -4,7 +4,7 @@ if exist c:\nginx\nginx.exe (
   if not exist "C:\nginx\conf\conf.d" mkdir C:\nginx\conf\conf.d
   xcopy vasya.conf C:\nginx\conf\conf.d /y
 ) else (
-  tar xf ./nginx-1.24.0.zip
+  powershell -command "Expand-Archive -Force 'nginx-1.24.0.zip' '.'"
   mkdir C:\nginx
   xcopy nginx-1.24.0\* C:\nginx\ /e /s /y
   mkdir C:\nginx\conf\conf.d
