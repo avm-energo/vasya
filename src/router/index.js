8f88737873a9ae00f689db04714e953a6f64ee8b
chopas11 <alexander.nikiforov.02@gmail.com>
2024-11-14 18:21:23 +0300

    При старте Васи автоматически входить под гостя

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Main from '../components/Main.vue'
 import Version from '../components/Version.vue'
 import Authorization from '@/pages/Authorization.vue'
+import {login} from "@/actions/AuthorizationActions";
 
 const routes = [
   {
@@ -35,8 +36,10 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach(function (to, from, next) {
+router.beforeEach(async function (to, from, next) {
   let isAuth = !!localStorage.getItem('token');
+  console.log("isAuth = ", isAuth)
+  console.log("to = ", to)
   let role = localStorage.getItem('role')
   let name = 'АСУ-ВЭИ'
   if (to.meta.title) {
@@ -44,12 +47,18 @@ router.beforeEach(function (to, from, next) {
   } else {
     document.title = name;
   }
+  if (to.name === 'Authorization' && !isAuth) {
+    await login('Guest', 'Guest', (e) =>{})
+  }
   if (to.name === 'Authorization' && isAuth) {
     router.push('/sonica');
+    console.log("Сразу входим")
     return;
   }
   if (to.name !== 'Authorization') {
     if (!isAuth) {
+      console.log("Переходим на страницу вавторизации")
+      // await login('Guest', 'Guest', (e) =>{})
       router.push('/authorization');
       return;
     }
